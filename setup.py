4f7fc582bf75e3a7ad2e82b61abd6cae5d9c0941
Yipeng Sha <ys2664@nyu.edu>
2019-11-18 15:50:05 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env/python
+from setuptools import setup
+
+setup(name='compedesign', version='1.0', description='CompE Stub Library', authors='Tejus Gowda, Yipeng Sha',
+ author_email='trg279@nyu.edu, ys2664@nyu.edu', url='https://github.com/ysppppp', py_modules=['compedesign'], install_requires=['pyserial', 'Adafruit_BBIO'])
